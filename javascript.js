390b5c5d9d92cd3b3c0db7d8104f88a41ac5b18b
dafab8 <dafinaab1@gmail.com>
2025-06-07 17:43:29 +0200

    cards added to html

--- javascript.js
+++ javascript.js
@@ -1,1 +1,102 @@
+let products = [
+  {
+    name: "Take the dog for a walk",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+    PriorityLevel: 0,
+    deadline: "27.01.2022",
+  },
 
+  {
+    name: "Take the dog for a walk",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+    PriorityLevel: 0,
+    deadline: "27.01.2022",
+  },
+
+  {
+    name: "Take the dog for a walk",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+    PriorityLevel: 0,
+    deadline: "27.01.2022",
+  },
+
+  {
+    name: "Take the dog for a walk",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+    PriorityLevel: 0,
+    deadline: "27.01.2022",
+  },
+
+  {
+    name: "Take the dog for a walk",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+    PriorityLevel: 0,
+    deadline: "27.01.2022",
+  },
+
+  {
+    name: "Take the dog for a walk",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+    PriorityLevel: 0,
+    deadline: "27.01.2022",
+  },
+
+  {
+    name: "Take the dog for a walk",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+    PriorityLevel: 0,
+    deadline: "27.01.2022",
+  },
+
+  {
+    name: "Take the dog for a walk",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+    PriorityLevel: 0,
+    deadline: "27.01.2022",
+  },
+
+  {
+    name: "Take the dog for a walk",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+    PriorityLevel: 0,
+    deadline: "27.01.2022",
+  },
+];
+
+for (item of products) {
+  document.getElementById("result").innerHTML += `
+    <div>
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${item.name}</h5>
+                <p class="card-text">Some quick example text to build on the card title and 
+                make up the bulk of the card’s content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+</div>
+
+
+
+    
+    `;
+}
