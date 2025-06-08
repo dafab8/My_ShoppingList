0916d13b0006f1b68bf6f1c9dd6cecfc60e9b317
dafab8 <dafinaab1@gmail.com>
2025-06-08 17:44:12 +0200

    .

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ let products = [
     name: "banana",
     description: "buy 4 banana",
     image:
-      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+      "https://cdn.pixabay.com/photo/2015/03/30/12/43/bananas-698608_1280.jpg",
     PriorityLevel: 0,
     deadline: "2025.05.27",
   },
@@ -12,7 +12,7 @@ let products = [
     name: "apple",
     description: "buy 5 apples",
     image:
-      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+      "https://cdn.pixabay.com/photo/2016/09/29/08/33/apple-1702316_1280.jpg",
     PriorityLevel: 0,
     deadline: "2025.07.31",
   },
@@ -21,7 +21,7 @@ let products = [
     name: "dates",
     description: "buy 1 kg of dates in the supermarket",
     image:
-      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+      "https://cdn.pixabay.com/photo/2021/09/19/19/18/dates-6638825_1280.jpg",
     PriorityLevel: 0,
     deadline: "2025.05.30",
   },
@@ -30,7 +30,7 @@ let products = [
     name: "garlic",
     description: "need to buy garlic",
     image:
-      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+      "https://cdn.pixabay.com/photo/2018/05/21/21/23/garlic-3419544_1280.jpg",
     PriorityLevel: 0,
     deadline: "2025.06.07",
   },
@@ -39,7 +39,7 @@ let products = [
     name: "carrots",
     description: "go buy carrots",
     image:
-      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+      "https://cdn.pixabay.com/photo/2016/08/26/21/16/carrot-juice-1623157_1280.jpg",
     PriorityLevel: 0,
     deadline: "2025.06.15",
   },
@@ -48,7 +48,7 @@ let products = [
     name: "salad",
     description: "salad for dinner",
     image:
-      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+      "https://cdn.pixabay.com/photo/2015/12/16/09/40/salad-1095649_1280.jpg",
     PriorityLevel: 0,
     deadline: "2025.06.20",
   },
@@ -56,8 +56,7 @@ let products = [
   {
     name: "pasta",
     description: "500 gramms of pasta ",
-    image:
-      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+    image: "https://cdn.pixabay.com/photo/2010/12/13/10/00/pasta-2093_1280.jpg",
     PriorityLevel: 0,
     deadline: "2025.06.29",
   },
@@ -66,7 +65,7 @@ let products = [
     name: "Milk",
     description: "2l of milk are needed",
     image:
-      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+      "https://cdn.pixabay.com/photo/2014/11/05/16/35/milk-518067_1280.jpg",
     PriorityLevel: 0,
     deadline: "2025.07.01",
   },
@@ -75,7 +74,7 @@ let products = [
     name: "cake",
     description: "cake for friends birthday",
     image:
-      "https://cdn.pixabay.com/photo/2017/06/21/22/32/planner-2428871_1280.jpg",
+      "https://cdn.pixabay.com/photo/2015/02/17/09/45/wedding-cake-639181_1280.jpg",
     PriorityLevel: 0,
     deadline: "2025.07.20",
   },
